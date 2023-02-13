22793937f4f3f1a592de872af84b31b10b30c077
leductai <ldtai26112002@gmail.com>
2023-02-13 14:27:28 +0700

    lesson8

--- lesson8/main.js
+++ lesson8/main.js
@@ -4,72 +4,43 @@ let email = document.querySelector('#email-input');
 let salary = document.querySelector('#salary-input');
 let city = document.querySelector('#city-input');
 let txtInput = document.querySelectorAll('.form__input__item input');
+let myTable = document.querySelector('.users_list');
 
+let users = [];
 
-let myTable = document.querySelector('#users');
-
-let users = [
-    {
-        id: 1,
-        fullName: 'Alex',
-        email: 'alex@gmail.com',
-        salary: 100,
-        city: 'San Francisco'
-    }
-];
-
-window.onload = () => {
-    renderUsers(users);
-};
-
-function renderUsers(arr) {
-    myTable.innerHTML = `
-        <tr>
-            <th>Full Name</th>
-            <th>Email</th>
-            <th>Salary</th>
-            <th>City</th>
-            <th>Actions</th>
-        </tr>
-    `;
-
-    arr.forEach((item,index) => {
-        myTable.innerHTML +=
-        `    
-        <tr>
-            <td>${item.fullName}</td>
-            <td>${item.email}</td>
-            <td>${item.salary}</td>
-            <td>${item.city}</td>
-            <td>
-                <button onclick="deleteUser(${index});">Delete</button>
-            </td>
-        </tr>
-        `;
-    });
-}
-
-function clearForm() {
-    txtInput.forEach((item) => {item.value = ""});
-}
+const renderRow = (item) => `    
+<tr>
+    <td>${item.fullName}</td>
+    <td>${item.email}</td>
+    <td>${item.salary}</td>
+    <td>${item.city}</td>
+    <td>
+        <button data-id=${item.id} onclick="deleteUser(this);">Delete</button>
+    </td>
+</tr>
+`;
 
 myForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    let obj = {
+
+    users.push({
         id: Date.now(),
         fullName: fullName.value,
         email: email.value,
         salary: salary.value,
         city: city.value
-    }
-    users.push(obj);
-    renderUsers(users);
+    });
+    myTable.innerHTML = users.reduce((res,item) => res + renderRow(item),'');
     clearForm();
 });
 
-function deleteUser(index) {
-    users.splice(index,1);
-    renderUsers(users);
+function deleteUser(element) {
+    element.parentElement.parentElement.remove();
+    users.splice(users.indexOf(x => x.id == element.dataset.id),1);
+}
+
+function clearForm() {
+    txtInput.forEach((item) => {item.value = ""});
 }
 
 
